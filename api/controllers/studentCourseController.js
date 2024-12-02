6e60263e3067831d79db6a286327eb97ace37965
Alessandra Bombardi <ale.brbr@gmail.com>
2024-12-02 12:42:35 -0300

    atualizando studentCourseController

--- api/controllers/studentCourseController.js
+++ api/controllers/studentCourseController.js
@@ -1,4 +1,4 @@
-const StudentCourse = require('../models/studentCourse');
+const StudentCourse = require("../models/studentCourse");
 
 /**
  * @swagger
@@ -9,7 +9,7 @@ const StudentCourse = require('../models/studentCourse');
 
 /**
  * @swagger
- * /student-courses:
+ * /student-course:
  *   post:
  *     summary: Cria uma nova associação entre aluno e curso
  *     tags: [Associação Aluno-Curso]
@@ -40,25 +40,27 @@ const StudentCourse = require('../models/studentCourse');
  *         description: Erro de validação ou dados ausentes
  */
 exports.createStudentCourse = async (req, res) => {
-    try {
-        // Validação simples dos dados recebidos
-        const { id_student, id_course } = req.body;
-        if (!id_student || !id_course) {
-            return res.status(400).json({ error: 'Os campos id_aluno e id_curso são obrigatórios.' });
-        }
-
-        // Criar a associação
-        const studentCourse = await StudentCourse.create(req.body);
-        res.status(201).json(studentCourse);
-    } catch (error) {
-        console.error('Erro ao criar associação:', error.message);
-        res.status(400).json({ error: error.message });
+  try {
+    // Validação simples dos dados recebidos
+    const { id_student, id_course } = req.body;
+    if (!id_student || !id_course) {
+      return res
+        .status(400)
+        .json({ error: "Os campos id_aluno e id_curso são obrigatórios." });
     }
+
+    // Criar a associação
+    const studentCourse = await StudentCourse.create(req.body);
+    res.status(201).json(studentCourse);
+  } catch (error) {
+    console.error("Erro ao criar associação:", error.message);
+    res.status(400).json({ error: error.message });
+  }
 };
 
 /**
  * @swagger
- * /student-courses:
+ * /student-course:
  *   get:
  *     summary: Retorna todas as associações de alunos e cursos
  *     tags: [Associação Aluno-Curso]
@@ -75,18 +77,18 @@ exports.createStudentCourse = async (req, res) => {
  *         description: Erro no servidor ao obter associações
  */
 exports.getStudentCourses = async (req, res) => {
-    try {
-        const studentCourses = await StudentCourse.findAll();
-        res.json(studentCourses);
-    } catch (error) {
-        console.error('Erro ao obter associações:', error.message);
-        res.status(500).json({ error: error.message });
-    }
+  try {
+    const studentCourses = await StudentCourse.findAll();
+    res.json(studentCourses);
+  } catch (error) {
+    console.error("Erro ao obter associações:", error.message);
+    res.status(500).json({ error: error.message });
+  }
 };
 
 /**
  * @swagger
- * /student-courses/{id_student}/{id_course}:
+ * /student-course/{id_student}/{id_course}:
  *   get:
  *     summary: Retorna uma associação específica pelo ID do aluno e do curso
  *     tags: [Associação Aluno-Curso]
@@ -116,22 +118,27 @@ exports.getStudentCourses = async (req, res) => {
  *         description: Erro no servidor ao obter associação
  */
 exports.getStudentCourseById = async (req, res) => {
-    const { id_student, id_course } = req.params; 
-    try {
-        const studentCourse = await StudentCourse.findOne({ where: { id_student, id_course } });
-        if (!studentCourse) {
-            return res.status(404).json({ error: 'Associação não encontrada.' });
-        }
-        res.json(studentCourse);
-    } catch (error) {
-        console.error('Erro ao obter associação:', error.message);
-        res.status(500).json({ error: error.message });
+  const { id_student, id_course } = req.params;
+  try {
+    const studentCourse = await StudentCourse.findOne({
+      where: {
+        StudentIdStudent: id_student,
+        CourseIdCourse: id_course,
+      },
+    });
+    if (!studentCourse) {
+      return res.status(404).json({ error: "Associação não encontrada." });
     }
+    res.json(studentCourse);
+  } catch (error) {
+    console.error("Erro ao obter associação:", error.message);
+    res.status(500).json({ error: error.message });
+  }
 };
 
 /**
  * @swagger
- * /student-courses/{id_student}/{id_course}:
+ * /student-course/{id_student}/{id_course}:
  *   put:
  *     summary: Atualiza uma associação específica pelo ID do aluno e do curso
  *     tags: [Associação Aluno-Curso]
@@ -177,30 +184,38 @@ exports.getStudentCourseById = async (req, res) => {
  *         description: Associação não encontrada
  */
 exports.updateStudentCourse = async (req, res) => {
-    const { id_student, id_course } = req.params; 
-    try {
-        const studentCourse = await StudentCourse.findOne({ where: { id_student, id_course } });
-        if (!studentCourse) {
-            return res.status(404).json({ error: 'Associação não encontrada.' });
-        }
-
-        // Validação dos dados para atualização
-        const { id_student: newIdStudent, id_course: newIdCourse } = req.body; 
-        if (newIdStudent === undefined || newIdCourse === undefined) {
-            return res.status(400).json({ error: 'Os campos id_aluno e id_curso são obrigatórios para atualização.' });
-        }
-
-        await studentCourse.update(req.body);
-        res.json(studentCourse);
-    } catch (error) {
-        console.error('Erro ao atualizar associação:', error.message);
-        res.status(400).json({ error: error.message });
+  const { id_student, id_course } = req.params;
+  try {
+    const studentCourse = await StudentCourse.findOne({
+      where: {
+        StudentIdStudent: id_student,
+        CourseIdCourse: id_course,
+      },
+    });
+    if (!studentCourse) {
+      return res.status(404).json({ error: "Associação não encontrada." });
     }
+
+    // Validação dos dados para atualização
+    const { id_student: newIdStudent, id_course: newIdCourse } = req.body;
+    if (newIdStudent === undefined || newIdCourse === undefined) {
+      return res.status(400).json({
+        error:
+          "Os campos id_aluno e id_curso são obrigatórios para atualização.",
+      });
+    }
+
+    await studentCourse.update(req.body);
+    res.json(studentCourse);
+  } catch (error) {
+    console.error("Erro ao atualizar associação:", error.message);
+    res.status(400).json({ error: error.message });
+  }
 };
 
 /**
  * @swagger
- * /student-courses/{id_student}/{id_course}:
+ * /student-course/{id_student}/{id_course}:
  *   delete:
  *     summary: Deleta uma associação específica pelo ID do aluno e do curso
  *     tags: [Associação Aluno-Curso]
@@ -226,16 +241,21 @@ exports.updateStudentCourse = async (req, res) => {
  *         description: Erro no servidor ao deletar associação
  */
 exports.deleteStudentCourse = async (req, res) => {
-    const { id_student, id_course } = req.params; 
-    try {
-        const studentCourse = await StudentCourse.findOne({ where: { id_student, id_course } });
-        if (!studentCourse) {
-            return res.status(404).json({ error: 'Associação não encontrada.' });
-        }
-        await studentCourse.destroy();
-        res.status(204).send();
-    } catch (error) {
-        console.error('Erro ao deletar associação:', error.message);
-        res.status(500).json({ error: error.message });
+  const { id_student, id_course } = req.params;
+  try {
+    const studentCourse = await StudentCourse.findOne({
+      where: {
+        StudentIdStudent: id_student,
+        CourseIdCourse: id_course,
+      },
+    });
+    if (!studentCourse) {
+      return res.status(404).json({ error: "Associação não encontrada." });
     }
+    await studentCourse.destroy();
+    res.status(204).send();
+  } catch (error) {
+    console.error("Erro ao deletar associação:", error.message);
+    res.status(500).json({ error: error.message });
+  }
 };
